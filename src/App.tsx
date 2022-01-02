1218c352dc2f97f8fdd2da33e2b12a90d1907efa
Ricardo Granvilla <rgranvilla@gmail.com>
2022-01-02 19:28:40 -0300

    global styling aplication

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <div className="App">
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </div>
   );
 }
